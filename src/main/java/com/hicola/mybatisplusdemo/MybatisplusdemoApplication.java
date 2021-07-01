c2f53851be0342439dfd3469e90e49b7946cea73
by19880401 <willis.bai@outlook.com>
2021-07-01 15:27:41 +0800

    环境配置改动

---
+++ src/main/java/com/hicola/mybatisplusdemo/MybatisplusdemoApplication.java
@@ -0,0 +1,13 @@
+package com.hicola.mybatisplusdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisplusdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisplusdemoApplication.class, args);
+    }
+
+}

a177c820b1a5a26977f40b61c4099660d9b228a0
by19880401 <willis.bai@outlook.com>
2021-07-22 15:16:42 +0800

    集成mybatis-plus成功

--- src/main/java/com/hicola/utils/ObjectUtils.java
+++ src/main/java/com/hicola/utils/ObjectUtils.java
@@ -6,16 +6,14 @@ import org.apache.commons.lang3.StringUtils;
 import java.lang.reflect.Field;
 
 /**
+ * 在effective java（这本书真的很棒）中说道，最佳的单例实现模式就是枚举模式。利用枚举的特性，让JVM来帮我们保证线程安全和单一实例的问题。除此之外，写法还特别简单
+ *
  * @author bai.yang email:willis.bai@outlook.com
  * @date 2021/7/22
  * @
  */
 public enum ObjectUtils {
     INSTANCE;
-
-    ObjectUtils() {
-    }
-
 
     /**
      * 判断对象中的所有属性是否都为null
